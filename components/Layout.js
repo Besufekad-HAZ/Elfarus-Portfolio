50c6cd876ae90a7035ed6cf44c7de062a8b702b0
Besufekad-HAZ <besurye@gmail.com>
2024-08-01 15:45:04 +0300

    Use prettier rc and font files

--- components/Layout.js
+++ components/Layout.js
@@ -1,5 +1,12 @@
 // fonts
-import {Sora} from '@next/font/google';
+import { Sora } from "@next/font/google";
+
+// font settings
+const sora = Sora({
+  subsets: ["latin"],
+  variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
 
 const Layout = () => {
   return <div>Layout</div>;
