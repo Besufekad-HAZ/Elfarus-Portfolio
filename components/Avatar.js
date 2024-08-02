8f03519fd8bcce5603e70f9aa90e675d0bd490e1
Besufekad-HAZ <besurye@gmail.com>
2024-08-02 14:22:07 +0300

    Save the latest avatart and other changes

--- components/Avatar.js
+++ components/Avatar.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 
 const Avatar = () => {
   return (
-    <div className="hidden xl:flex xl:max-w-none">
+    <div className="hidden xl:flex xl:max-w-none xl:h-full xl:items-end xl:justify-center">
       <Image
-        src={"/avatar.png"}
-        width={737}
-        height={678}
+        src={"/Suree.png"}
+        width={517}
+        height={478}
         alt="Avatar image"
-        className="translate-z-0 w-full h-full"
+        className="translate-z-0 w-auto h-auto max-w-[90%] max-h-[90%] mix-blend-"
       />
     </div>
   );
