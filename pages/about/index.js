7de8d398f358dc2ede59a5eae2f4bdc61546634c
Besufekad-HAZ <besurye@gmail.com>
2024-08-09 17:32:39 +0300

    Finish implementing the experience section

--- pages/about/index.js
+++ pages/about/index.js
@@ -141,10 +141,12 @@ const About = () => {
                 className="relative flex-1 after:w-[1px] after:h-full
               after:bg-white/10 after:absolute after:top-0 after:right-0"
               >
-                <div className="text-2xl xl-text-4xl font-extrabold text-accent mb-2">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={10} duration={5} /> +
                 </div>
-                <div>Years of experience</div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Years of experience
+                </div>
               </div>
             </div>
           </div>
