5e3c4a7eb0be15007294113d94d3d35c6b1153a4
Besufekad-HAZ <besurye@gmail.com>
2024-08-09 15:47:31 +0300

    Implement some styling

--- pages/about/index.js
+++ pages/about/index.js
@@ -99,6 +99,7 @@ const aboutData = [
 
 const About = () => {
   const [index, setIndex] = useState(0);
+  console.log(index);
   return (
     <div className="h-full bg-primary/30 py-32 text-center xl:text-left">
       <Circles />
@@ -113,17 +114,31 @@ const About = () => {
         <Avatar />
       </motion.div>
       <div className="container mx-auto h-full flex flex-col items-center xl:flex-row gap-x-6">
-        <div>text</div>
+        <div className="flex-1 flex flex-col justify-center">text</div>
         <div>
-          <div>
+          <div className="flex gap-x-4 xl-gap-x-8 mx-auto xl-mx-0 mb-4">
             {aboutData.map((item, itemIndex) => {
               return (
                 <div
-                  key={itemsIndex}
-                  className="cursor-pointer capitalize xl:text-lg relative after:w-8
-              after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0 "
+                  key={itemIndex}
+                  className={`${
+                    index === itemIndex &&
+                    "text-accent after:w-[100%] after:bg-accent after:transition-all after:duration-300"
+                  } cursor-pointer capitalize xl:text-lg relative after:w-8
+              after:h-[2px] after:bg-white after:absolute after:-bottom-1 after:left-0`}
+                  onClick={() => setIndex(itemIndex)}
                 >
                   {item.title}
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            {aboutData[index].info.map((item, itemIndex) => {
+              return (
+                <div key={itemIndex}>
+                  {/* title */}
+                  <div>{item.title}</div>
                 </div>
               );
             })}
