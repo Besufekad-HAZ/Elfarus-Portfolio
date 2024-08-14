3fc7f46d3767bff1c7720150118c72e82a4b6480
Besufekad-HAZ <besurye@gmail.com>
2024-08-14 12:49:31 +0300

    Start working on the services page and import neccesary modules and components

--- pages/services/index.js
+++ pages/services/index.js
@@ -1,5 +1,17 @@
+import ServiceSlider from "../../components/ServiceSlider";
+import Bulb from "../../components/Bulb";
+import Circles from "../../components/Circles";
+
+// framer motion
+import { motion } from "framer-motion";
+import { fadeIn } from "../../variants";
+
 const Services = () => {
-  return <div>Services</div>;
+  return <div>
+    <Circles />
+    <ServiceSlider />
+    <Bulb />
+  </div>;
 };
 
 export default Services;
