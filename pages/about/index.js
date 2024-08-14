e9662a36deff911975f928cedc87add24b3275e8
Besufekad-HAZ <besurye@gmail.com>
2024-08-14 11:10:39 +0300

    Add Satisfied Clients and Finished projects section inside the about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -148,6 +148,30 @@ const About = () => {
                   Years of experience
                 </div>
               </div>
+              {/* Clients */}
+              <div
+                className="relative flex-1 after:w-[1px] after:h-full
+              after:bg-white/10 after:absolute after:top-0 after:right-0"
+              >
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={100} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Satisfied Clients
+                </div>
+              </div>
+              {/* Projects */}
+              <div
+                className="relative flex-1 after:w-[1px] after:h-full
+              after:bg-white/10 after:absolute after:top-0 after:right-0"
+              >
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
+                  <CountUp start={0} end={200} duration={5} /> +
+                </div>
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                  Finished Projects
+                </div>
+              </div>
             </div>
           </div>
         </div>
