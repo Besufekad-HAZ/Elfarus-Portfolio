71a4662aa81b467c945cb55999bf5fa44df6b156
Besufekad-HAZ <besurye@gmail.com>
2024-08-14 13:03:25 +0300

    Update the bubl component page

--- components/Bulb.js
+++ components/Bulb.js
@@ -4,8 +4,8 @@ import Image from "next/image";
 const Bulb = () => {
   return (
     <div
-      className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse duration-75
-    z-10 w-[200px] xl:width=[]"
+      className="absolute -left-36 -bottom-12 rotate-12 mix-blend-color-dodge animate-pulse
+      duration-75 z-10 w-[200px] xl:width=[200px] xl:w-[260px]"
     >
       <Image
         src={"/bulb.png"}
