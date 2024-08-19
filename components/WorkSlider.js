01ebc2dc1fbd237480f89c2a1abc61ab30f1e623
Besufekad-HAZ <besurye@gmail.com>
2024-08-19 14:12:14 +0300

    Update the wholde styling of the works section

--- components/WorkSlider.js
+++ components/WorkSlider.js
@@ -73,11 +73,17 @@ const WorkSlider = () => {
       {workSlider.slides.map((slide, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="grid grid-cols-2 grid-rows-2 gap-4">
+            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
               {slide.images.map((image, index) => {
                 return (
-                  <div>
-                    <div>
+                  <div
+                    className="relative rounded-lg overflow-hidden flex
+                  items-center justify-center group"
+                  >
+                    <div
+                      className="flex items-center justify-center relative
+                    oveflow-hidden group"
+                    >
                       {/* image */}
                       <Image
                         src={image.path}
@@ -85,6 +91,38 @@ const WorkSlider = () => {
                         height={300}
                         alt="work slides image "
                       />
+                      {/* overlay gradient */}
+                      <div
+                        className="absolute inset-0 bg-gradient-to-l
+                      from-transparent via-[#e838cc] to-[#4a22bd] opacity-0
+                      group-hover:opacity-80 transition-all duration-700"
+                      ></div>
+                      {/* title */}
+                      <div
+                        className="absolute bottom-0 translate-y-full
+                      group-hover:-translate-y-10 group-hover:xl:-translate-y-20
+                      transition-all duration-300"
+                      >
+                        <div
+                          className="flex items-center gap-x-2
+                        text-[13px] tracking-[0.2em]"
+                        >
+                          {/* title part 1  */}
+                          <div className="delay-100">LIVE</div>
+
+                          {/* title part 2  */}
+                          <div
+                            className="translate-y-[500%] group-hover:translate-y-0 transition-all
+                          duration-300 delay-100"
+                          >
+                            PROJECT
+                          </div>
+                          {/* icon */}
+                          <div>
+                            <BsArrowRight />
+                          </div>
+                        </div>
+                      </div>
                     </div>
                     {/* <BsArrowRight /> */}
                   </div>
