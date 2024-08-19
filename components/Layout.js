4ee2eea2fc48c7a1df3ed5576d797bd455af1e82
Besufekad-HAZ <besurye@gmail.com>
2024-08-19 14:49:23 +0300

    Fix some styling and font errors throughout the Project tailwind and next.js config

--- components/Layout.js
+++ components/Layout.js
@@ -1,10 +1,16 @@
 // fonts
-import { Sora } from "@next/font/google";
+import { Sora, Poppins } from "@next/font/google";
 
 // font settings
 const sora = Sora({
   subsets: ["latin"],
   variable: "--font-sora",
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
+});
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  variable: "--font-poppins",
   weight: ["100", "200", "300", "400", "500", "600", "700", "800"],
 });
 
--- pages/about/index.js
+++ pages/about/index.js
@@ -159,10 +159,10 @@ const About = () => {
                 className="relative flex-1 after:w-[1px] after:h-full
               after:bg-white/10 after:absolute after:top-0 after:right-0"
               >
-                <div className="text-2xl xl:text-4xl -extrabold text-accent mb-2">
+                <div className="text-2xl xl:text-4xl font-extrabold text-accent mb-2">
                   <CountUp start={0} end={10} duration={5} /> +
                 </div>
-                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px]">
+                <div className="text-xs uppercase tracking-[1px] leading-[1.4] max-w-[100px] mb-2">
                   Years of experience
                 </div>
               </div>
