c09c556b694203406ffb19a0c46847520e24930f
Besufekad-HAZ <besurye@gmail.com>
2024-08-20 12:00:27 +0300

    Work on the styling of the testimonial slider page

--- components/TestimonialSlider.js
+++ components/TestimonialSlider.js
@@ -1,25 +1,25 @@
 // testimonial data
-const testimonialData = [
+const testimonialSlider = [
   {
     image: "/t-avt-1.png",
     name: "Anne Smith",
     position: "Customer",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+      "The video editing and graphic design work was exceptional. Truly brought our vision to life!",
   },
   {
     image: "/t-avt-2.png",
     name: "Jane Doe",
     position: "Customer",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+      "Outstanding videography! Captured every moment perfectly. Highly recommend for any project.",
   },
   {
     image: "/t-avt-3.png",
-    name: "Jhon Doe",
+    name: "John Doe",
     position: "Customer",
     message:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum expedita odit beatae, cupiditate saepe quam officia aut placeat quas neque!",
+      "Professional and creative. The final product exceeded our expectations. Great job!",
   },
 ];
 
@@ -35,12 +35,12 @@ import "swiper/css/pagination";
 import { Pagination } from "swiper";
 
 // icons
-import { BsArrowRight } from "react-icons/bs";
+import { FaQuoteLeft } from "react-icons/fa";
 
 // import next image
 import Image from "next/image";
 
-const WorkSlider = () => {
+const TestimonialSlider = () => {
   return (
     <Swiper
       spaceBetween={10}
@@ -50,70 +50,28 @@ const WorkSlider = () => {
       modules={[Pagination]}
       className="h-[280px] sm:h-[480px]"
     >
-      {workSlider.slides.map((slide, index) => {
+      {testimonialSlider.map((person, index) => {
         return (
           <SwiperSlide key={index}>
-            <div className="grid grid-cols-2 grid-rows-2 gap-4 cursor-pointer">
-              {slide.images.map((image, index) => {
-                return (
-                  <a
-                    href={image.link}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    key={index}
-                    className="relative rounded-lg overflow-hidden flex
-                      items-center justify-center group"
-                  >
-                    <div
-                      className="flex items-center justify-center relative
-                        overflow-hidden group"
-                    >
-                      {/* image */}
-                      <Image
-                        src={image.path}
-                        width={500}
-                        height={300}
-                        alt="work slides image"
-                      />
-                      {/* overlay gradient */}
-                      <div
-                        className="absolute inset-0 bg-gradient-to-l
-                          from-transparent via-[#e838cc] to-[#4a22bd] opacity-0
-                          group-hover:opacity-80 transition-all duration-700"
-                      ></div>
-                      {/* title */}
-                      <div
-                        className="absolute bottom-0 translate-y-full
-                          group-hover:-translate-y-10 group-hover:xl:-translate-y-20
-                          transition-all duration-300"
-                      >
-                        <div
-                          className="flex items-center gap-x-2
-                            text-[13px] tracking-[0.2em]"
-                        >
-                          {/* title part 1  */}
-                          <div className="delay-100">LIVE</div>
-
-                          {/* title part 2  */}
-                          <div
-                            className="translate-y-[500%] group-hover:translate-y-0 transition-all
-                              duration-300 delay-100"
-                          >
-                            PROJECT
-                          </div>
-                          {/* icon */}
-                          <div
-                            className="text-xl translate-y-[500%] group-hover:translate-y-0
-                              transition-all duration-300 delay-200"
-                          >
-                            <BsArrowRight />
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </a>
-                );
-              })}
+            <div>
+              {/* avatar, name, position */}
+              <div>
+                {/* avatar  */}
+                <img src={person.image} alt="" />
+                {/* name */}
+                <div className="text-xl font-semibold">{person.name}</div>
+                {/* position  */}
+                <div className="text-sm">{person.position}</div>
+              </div>
+              {/* quote & message */}
+              <div>
+                {/* quote icon */}
+                <div>
+                  <FaQuoteLeft className="text-4xl" />
+                </div>
+                {/* message  */}
+                <div className="text-lg font-semibold">{person.message}</div>
+              </div>
             </div>
           </SwiperSlide>
         );
@@ -122,4 +80,4 @@ const WorkSlider = () => {
   );
 };
 
-export default WorkSlider;
+export default TestimonialSlider;
