63ef50842d65c664dea6a32ebb9612dd03f977bc
Besufekad-HAZ <besurye@gmail.com>
2024-08-20 12:39:22 +0300

    Import fadeIn variant

--- pages/testimonials/index.js
+++ pages/testimonials/index.js
@@ -4,12 +4,15 @@ import TestimonialSlider from "../../components/TestimonialSlider";
 // import framer motion
 import { motion } from "framer-motion";
 
+// import variants
+import { fadeIn } from "../../variants";
+
 const Testimonials = () => {
   return (
     <div className="h-full bg-primary/30 py-32 text-center">
       <div className="container mx-auto h-full flex flex-col justify-center">
         {/* title  */}
-        <motion.h2 className="h2 mb-8 xl:mb-0">
+        <motion.h2 variants={fade} className="h2 mb-8 xl:mb-0">
           What clients <span className="text-accent">say.</span>
         </motion.h2>
 
