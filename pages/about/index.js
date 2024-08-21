109399af83d86bdb3600197679bd38169adf5c54
Besufekad-HAZ <besurye@gmail.com>
2024-08-21 14:20:39 +0300

    Fix eslint error 1

--- pages/about/index.js
+++ pages/about/index.js
@@ -244,9 +244,11 @@ const About = () => {
                   <div>{item.stage}</div>
                   <div className="flex gap-x-4">
                     {/* icons */}
-                    {item.icons?.map((icon, itemIndex) => {
-                      return <div className="text-2xl text-white">{icon}</div>;
-                    })}
+                    {item.icons?.map((icon, iconIndex) => (
+                      <div key={iconIndex} className="text-2xl text-white">
+                        {icon}
+                      </div>
+                    ))}
                   </div>
                 </div>
               );
