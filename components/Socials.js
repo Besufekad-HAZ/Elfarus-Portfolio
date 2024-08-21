1b339f578a0ebfa2db860eb93abd89682b480f04
Besufekad-HAZ <besurye@gmail.com>
2024-08-21 22:04:21 +0300

    Fix using the <Link> component from Next.js with a child <a> element using the legacyBehavior

--- components/Socials.js
+++ components/Socials.js
@@ -1,4 +1,3 @@
-// next link
 import Link from "next/link";
 
 //icons
@@ -10,52 +9,59 @@ import {
   RiBehanceLine,
   RiPinterestLine,
 } from "react-icons/ri";
+
 const Socials = () => {
   return (
     <div className="flex items-center gap-x-5 text-xl">
-      <Link href="https://www.youtube.com/@ELFARUS7" passHref>
+      <Link href="https://www.youtube.com/@ELFARUS7" legacyBehavior passHref>
         <a
           target="_blank"
+          rel="noopener noreferrer"
           className="hover:text-accent transition-all duration-300"
         >
           <RiYoutubeLine />
         </a>
       </Link>
-      <Link href="https://www.facebook.com/elfarus7" passHref>
+      <Link href="https://www.facebook.com/elfarus7" legacyBehavior passHref>
         <a
           target="_blank"
+          rel="noopener noreferrer"
           className="hover:text-accent transition-all duration-300"
         >
           <RiFacebookLine />
         </a>
       </Link>
-      <Link href="https://www.instagram.com/elfarus7" passHref>
+      <Link href="https://www.instagram.com/elfarus7" legacyBehavior passHref>
         <a
           target="_blank"
+          rel="noopener noreferrer"
           className="hover:text-accent transition-all duration-300"
         >
           <RiInstagramLine />
         </a>
       </Link>
-      <Link href="" passHref>
+      <Link href="" legacyBehavior passHref>
         <a
           target="_blank"
+          rel="noopener noreferrer"
           className="hover:text-accent transition-all duration-300"
         >
           <RiDribbbleLine />
         </a>
       </Link>
-      <Link href="" passHref>
+      <Link href="" legacyBehavior passHref>
         <a
           target="_blank"
+          rel="noopener noreferrer"
           className="hover:text-accent transition-all duration-300"
         >
           <RiBehanceLine />
         </a>
       </Link>
-      <Link href="" passHref>
+      <Link href="" legacyBehavior passHref>
         <a
           target="_blank"
+          rel="noopener noreferrer"
           className="hover:text-accent transition-all duration-300"
         >
           <RiPinterestLine />
