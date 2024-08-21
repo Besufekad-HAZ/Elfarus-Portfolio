1cf62d0bf5114bc4513a6ceafc2d946b8d97a8d5
Besufekad-HAZ <besurye@gmail.com>
2024-08-21 12:06:43 +0300

    Import neccesary packages inside the contact index page

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,6 +1,15 @@
 // components
 import Circles from "../../components/Circles";
 
+// icons
+import { BsArrowRight } from "react-icons/bs";
+
+// framer motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "../../variants";
+
 const Contact = () => {
   return <div>Contact</div>;
 };
